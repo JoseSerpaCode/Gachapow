219a695bb9ed783ba74a579760653638434844e0
José Serpa Medina <joseserpamedinaxd@gmail.com>
2025-11-22 22:38:23 -0500

    Add configuration file check in main function

--- src/main.c
+++ src/main.c
@@ -2,8 +2,21 @@
 #include "hw_api.h"
 #include "state_manager.h"
 
+#include <direct.h>
+#include <stdio.h>
+#include "assets.h"
+
 int main(void)
 {
+    // Comprobamos si el archivo de config existe
+    FILE *file = fopen("assets/data/config.json", "r");
+    if (!file) {
+        // No existe: estamos probablemente en /build, así que subimos un nivel
+        _chdir("..");
+    } else {
+        fclose(file);
+    }
+
     hw_init();
 
     StateManager_Init(STATE_INTRO);
